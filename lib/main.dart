a02f9e6387d117865cbedd03196c16c8b1b3e30d
jangho <seojh7816@gmail.com>
2025-01-02 18:33:45 +0900

    [ui feat] header

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
 
+class Player {
+  String? name;
+
+  Player({required this.name});
+}
 void main() {
+  var jangho = Player(name: "jangho");
+
   runApp(App());
 }
 
@@ -11,12 +18,36 @@ class App extends StatelessWidget {
     
     return MaterialApp(
       home :Scaffold(
-        appBar: AppBar(
-          centerTitle:  false,
-          title : Text('Hello Flutter'),
-        ),
-        body : Center(child: Text('Hello world'),)
+        backgroundColor: Color(0xFF181818)
+        ,
+        body : Padding(padding : EdgeInsets.symmetric(horizontal: 40),
+        child : Column(
+          children: [
+            SizedBox(
+              height : 80
+            ),
+            Row(mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+              Text('Hey, Selena', 
+                style: TextStyle(
+                color : Colors.white,
+                fontSize : 28 ,
+                fontWeight : FontWeight.w800),
+                  ),
+              Text('Welcome back',
+                style : TextStyle(color: Colors.white.
+                withOpacity(0.8),
+                fontSize: 18)),
+            ],
+            )
+            ],)
+          ],
+        )
       ),
+      )
     );
   }
 }
